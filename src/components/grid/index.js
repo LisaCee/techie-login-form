fca80209b968ceb2784202a0bce9b4966ab538c9
lisacee <lisaceedev@gmail.com>
2024-07-04 16:24:15 -0700

    Create mock login form
    Create mock login form based on [this example](https://codepen.io/ayush602/full/mdQJreW)

---
+++ src/components/grid/index.js
@@ -0,0 +1,44 @@
+import './styles.scss';
+
+
+export function Grid() {
+	return (
+		<>
+			<div id="grid">
+				<div className="grid-item"/>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+				<div className="grid-item"></div>
+
+			</div>
+		</>
+	)
+}
+
+
